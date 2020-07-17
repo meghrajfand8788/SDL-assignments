451f5a8a9bd3cce832158752ee7133c11854182b
vijayshete <67770267+vijayshete@users.noreply.github.com>
2020-07-17 11:43:22 +0530

    Create Ass 1 b.py

---
+++ Ass 1 b.py
@@ -0,0 +1,14 @@
+list=[5,8,1,7,3,9,4]
+print("List of integers created :  ",list)
+list[2]=6
+print("Int added at position 2 :  ",list)
+list.remove(9)
+print("Int 9 removed :  ",list)
+list.append(11)
+print("Int 11 added at end :  ", list)
+list.sort()
+print("List sorted :  ",list)
+list.pop()
+print("Last int removed :  ",list)
+list.reverse()
+print("List reversed :  ",list)
