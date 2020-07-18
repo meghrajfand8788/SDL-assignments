06cd563387dd038d457a47be1ca1276b8abd48cb
vijayshete <67770267+vijayshete@users.noreply.github.com>
2020-07-18 13:00:02 +0530

    Update Ass 1 b.py

--- Ass 1 b.py
+++ Ass 1 b.py
@@ -1,14 +1,16 @@
-list=[5,8,1,7,3,9,4]
-print("List of integers created :  ",list)
-list[2]=6
-print("Int added at position 2 :  ",list)
+list=[]
+print("Empty list of integers created :  ",list)
+list.insert(0,5)
+list.insert(1,7)
+list.insert(0,9)
+print("After addition  tree integers :  ",list)
 list.remove(9)
-print("Int 9 removed :  ",list)
 list.append(11)
-print("Int 11 added at end :  ", list)
+list.append(4)
+print(list)
 list.sort()
 print("List sorted :  ",list)
 list.pop()
-print("Last int removed :  ",list)
+print("After pop operation :  ",list)
 list.reverse()
 print("List reversed :  ",list)
