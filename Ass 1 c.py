e74ea56c6a8ab8157cc8911a4d8a76541e32298e
vijayshete <67770267+vijayshete@users.noreply.github.com>
2020-07-18 12:07:39 +0530

    Add files via upload

---
+++ Ass 1 c.py
@@ -0,0 +1,5 @@
+a=int(input("Enter a no : "))
+b=int(input("Enter second no : "))
+print("Addition of two no is : ",a+b)
+print("Difference of two integers is : ",a-b)
+print("Product of two no is :",a*b)
